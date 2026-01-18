c6c08d35068b04d23808e7e82248a6c80ed71227
VoiciD <dany271201@gmail.com>
2026-01-18 17:34:31 +0100

    New Documentation and new sdr software

--- GNURadio/SDR_Software.py
+++ GNURadio/SDR_Software.py
@@ -73,7 +73,7 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         self.demod = demod = 0
         self.channel_frequency = channel_frequency = 39.9976e6
         self.center_frequency = center_frequency = 40e6
-        self.bandwidth = bandwidth = 125e3
+        self.bandwidth = bandwidth = 2.7e3
 
         ##################################################
         # Blocks
@@ -104,7 +104,7 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         # Create the options list
         self._demod_options = [0, 1, 2, 3, 4]
         # Create the labels list
-        self._demod_labels = ['FM', 'SSB', 'CW', 'Test SSB', 'Test SSB Filter Method']
+        self._demod_labels = ['FM', 'Do not use (SSB)', 'CW', 'Do not use (SSB)', 'SSB']
         # Create the combo box
         self._demod_tool_bar = Qt.QToolBar(self)
         self._demod_tool_bar.addWidget(Qt.QLabel("Demodulation" + ": "))
@@ -123,7 +123,7 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         self._center_frequency_range = qtgui.Range(30e6, 120e6, 1, 40e6, 200)
         self._center_frequency_win = qtgui.RangeWidget(self._center_frequency_range, self.set_center_frequency, "Center Frequency", "eng_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._center_frequency_win)
-        self._bandwidth_range = qtgui.Range(1, 200e3, 1, 125e3, 200)
+        self._bandwidth_range = qtgui.Range(1, 200e3, 1, 2.7e3, 200)
         self._bandwidth_win = qtgui.RangeWidget(self._bandwidth_range, self.set_bandwidth, "Filter Bandwidth", "eng_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._bandwidth_win)
         self.rational_resampler_xxx_1 = filter.rational_resampler_ccc(
@@ -132,8 +132,8 @@ class SDR_Software(gr.top_block, Qt.QWidget):
                 taps=[],
                 fractional_bw=0)
         self.rational_resampler_xxx_0_0_0_0 = filter.rational_resampler_ccc(
-                interpolation=12,
-                decimation=250,
+                interpolation=6,
+                decimation=125,
                 taps=[],
                 fractional_bw=0)
         self.rational_resampler_xxx_0_0_0 = filter.rational_resampler_ccc(
@@ -156,7 +156,7 @@ class SDR_Software(gr.top_block, Qt.QWidget):
             window.WIN_BLACKMAN_hARRIS, #wintype
             center_frequency, #fc
             samp_rate, #bw
-            "", #name
+            "HF-Spektrum", #name
             1, #number of inputs
             None # parent
         )
@@ -194,8 +194,8 @@ class SDR_Software(gr.top_block, Qt.QWidget):
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
             0, #fc
-            samp_rate, #bw
-            "", #name
+            48e3, #bw
+            "Basisband Spektrum", #name
             1,
             None # parent
         )
@@ -238,7 +238,7 @@ class SDR_Software(gr.top_block, Qt.QWidget):
             window.WIN_BLACKMAN_hARRIS, #wintype
             center_frequency, #fc
             samp_rate, #bw
-            "", #name
+            "HF-Spektrum", #name
             1,
             None # parent
         )
@@ -295,13 +295,13 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         self.osmosdr_source_0.set_bb_gain(0, 0)
         self.osmosdr_source_0.set_antenna('QO-100', 0)
         self.osmosdr_source_0.set_bandwidth(samp_rate, 0)
-        self.low_pass_filter_2_0 = filter.fir_filter_ccf(
+        self.low_pass_filter_2 = filter.fir_filter_ccf(
             1,
             firdes.low_pass(
                 1,
-                1e6,
+                48e3,
                 bandwidth,
-                500,
+                150,
                 window.WIN_HAMMING,
                 6.76))
         self.low_pass_filter_1_0 = filter.fir_filter_fff(
@@ -331,6 +331,7 @@ class SDR_Software(gr.top_block, Qt.QWidget):
                 25e3,
                 window.WIN_HAMMING,
                 6.76))
+        self.freq_xlating_fir_filter_xxx_0_0_0 = filter.freq_xlating_fir_filter_ccc(1, [20], 0, samp_rate)
         self.freq_xlating_fir_filter_xxx_0_0 = filter.freq_xlating_fir_filter_ccc(1, [22], 0, samp_rate)
         self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(1, [22], 0, samp_rate)
         self.blocks_swapiq_0_0 = blocks.swap_iq(1, gr.sizeof_gr_complex)
@@ -346,7 +347,6 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         self.blocks_selector_1.set_enabled(True)
         self.blocks_selector_0 = blocks.selector(gr.sizeof_gr_complex*1,0,demod)
         self.blocks_selector_0.set_enabled(True)
-        self.blocks_multiply_xx_2 = blocks.multiply_vcc(1)
         self.blocks_multiply_xx_1_0 = blocks.multiply_vff(1)
         self.blocks_multiply_xx_1 = blocks.multiply_vff(1)
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
@@ -357,14 +357,14 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
         self.blocks_complex_to_imag_0 = blocks.complex_to_imag(1)
         self.blocks_add_xx_0 = blocks.add_vff(1)
-        self.band_pass_filter_1 = filter.fir_filter_ccc(
+        self.band_pass_filter_0_0_0_0 = filter.fir_filter_ccc(
             1,
             firdes.complex_band_pass(
                 1,
                 48e3,
-                750,
-                1250,
-                500,
+                200,
+                (200+bandwidth),
+                150,
                 window.WIN_HAMMING,
                 6.76))
         self.band_pass_filter_0_0_0 = filter.fir_filter_ccc(
@@ -373,8 +373,8 @@ class SDR_Software(gr.top_block, Qt.QWidget):
                 1,
                 48e3,
                 200,
-                10e3,
-                500,
+                (200+bandwidth),
+                150,
                 window.WIN_HAMMING,
                 6.76))
         self.band_pass_filter_0_0 = filter.fir_filter_ccc(
@@ -383,7 +383,7 @@ class SDR_Software(gr.top_block, Qt.QWidget):
                 1,
                 48e3,
                 200,
-                10e3,
+                (200+bandwidth),
                 500,
                 window.WIN_HAMMING,
                 6.76))
@@ -398,7 +398,6 @@ class SDR_Software(gr.top_block, Qt.QWidget):
                 window.WIN_HAMMING,
                 6.76))
         self.audio_sink_0 = audio.sink(48000, '', True)
-        self.analog_sig_source_x_1_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, channel_frequency, 2, 0, 0)
         self.analog_sig_source_x_1_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, channel_frequency, 1, 0, 0)
         self.analog_sig_source_x_1 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, channel_frequency, 1, 0, 0)
         self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, (center_frequency-channel_frequency), 1, 0, 0)
@@ -411,10 +410,10 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         	gain=1.0,
         	tau=(75e-6),
         )
-        self.analog_agc2_xx_1_0_0 = analog.agc2_cc((1e-3), (1e-3), 1, 0.1, 65536)
+        self.analog_agc2_xx_1_0_0 = analog.agc2_cc((1e-3), 0.1, 0.5, 0.1, 65536)
         self.analog_agc2_xx_1_0 = analog.agc2_cc((1e-3), (1e-3), 0.1, 0.1, 65536)
         self.analog_agc2_xx_1 = analog.agc2_ff((1e-3), (1e-3), 0.1, 0.1, 65536)
-        self.analog_agc2_xx_0 = analog.agc2_cc((1e-3), (1e-3), 0.1, 0.1, 65536)
+        self.analog_agc2_xx_0 = analog.agc2_cc((1e-3), 0.1, 0.5, 0.1, 65536)
 
 
         ##################################################
@@ -423,16 +422,15 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         self.connect((self.analog_agc2_xx_0, 0), (self.blocks_complex_to_real_1, 0))
         self.connect((self.analog_agc2_xx_1, 0), (self.band_pass_filter_0, 0))
         self.connect((self.analog_agc2_xx_1_0, 0), (self.band_pass_filter_0_0, 0))
-        self.connect((self.analog_agc2_xx_1_0_0, 0), (self.band_pass_filter_0_0_0, 0))
+        self.connect((self.analog_agc2_xx_1_0_0, 0), (self.blocks_complex_to_real_1_0_0, 0))
         self.connect((self.analog_fm_demod_cf_0, 0), (self.blocks_selector_2, 0))
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
         self.connect((self.analog_sig_source_x_1, 0), (self.blocks_multiply_xx_1, 1))
         self.connect((self.analog_sig_source_x_1_0, 0), (self.blocks_multiply_xx_1_0, 1))
-        self.connect((self.analog_sig_source_x_1_0_0, 0), (self.blocks_multiply_xx_2, 1))
         self.connect((self.band_pass_filter_0, 0), (self.blocks_selector_2, 1))
         self.connect((self.band_pass_filter_0_0, 0), (self.blocks_complex_to_real_1_0, 0))
-        self.connect((self.band_pass_filter_0_0_0, 0), (self.blocks_complex_to_real_1_0_0, 0))
-        self.connect((self.band_pass_filter_1, 0), (self.analog_agc2_xx_0, 0))
+        self.connect((self.band_pass_filter_0_0_0, 0), (self.blocks_selector_1_0_0, 0))
+        self.connect((self.band_pass_filter_0_0_0_0, 0), (self.blocks_selector_1_0_0, 1))
         self.connect((self.blocks_add_xx_0, 0), (self.blocks_selector_1, 0))
         self.connect((self.blocks_complex_to_imag_0, 0), (self.blocks_multiply_xx_1_0, 0))
         self.connect((self.blocks_complex_to_real_0, 0), (self.blocks_multiply_xx_1, 0))
@@ -443,39 +441,39 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_selector_0, 0))
         self.connect((self.blocks_multiply_xx_1, 0), (self.low_pass_filter_1, 0))
         self.connect((self.blocks_multiply_xx_1_0, 0), (self.low_pass_filter_1_0, 0))
-        self.connect((self.blocks_multiply_xx_2, 0), (self.low_pass_filter_2_0, 0))
         self.connect((self.blocks_selector_0, 1), (self.blocks_complex_to_imag_0, 0))
         self.connect((self.blocks_selector_0, 1), (self.blocks_complex_to_real_0, 0))
-        self.connect((self.blocks_selector_0, 4), (self.blocks_multiply_xx_2, 0))
         self.connect((self.blocks_selector_0, 2), (self.freq_xlating_fir_filter_xxx_0, 0))
         self.connect((self.blocks_selector_0, 3), (self.freq_xlating_fir_filter_xxx_0_0, 0))
+        self.connect((self.blocks_selector_0, 4), (self.freq_xlating_fir_filter_xxx_0_0_0, 0))
         self.connect((self.blocks_selector_0, 0), (self.low_pass_filter_0, 0))
         self.connect((self.blocks_selector_1, 0), (self.rational_resampler_xxx_0_0, 0))
         self.connect((self.blocks_selector_1_0, 0), (self.rational_resampler_xxx_0_0_0, 0))
-        self.connect((self.blocks_selector_1_0_0, 0), (self.rational_resampler_xxx_0_0_0_0, 0))
+        self.connect((self.blocks_selector_1_0_0, 0), (self.analog_agc2_xx_1_0_0, 0))
         self.connect((self.blocks_selector_2, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.blocks_selector_2, 0), (self.qtgui_freq_sink_x_1, 0))
         self.connect((self.blocks_sub_xx_0, 0), (self.blocks_selector_1, 1))
         self.connect((self.blocks_swapiq_0, 0), (self.blocks_selector_1_0, 1))
-        self.connect((self.blocks_swapiq_0_0, 0), (self.blocks_selector_1_0_0, 1))
+        self.connect((self.blocks_swapiq_0_0, 0), (self.band_pass_filter_0_0_0_0, 0))
         self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.rational_resampler_xxx_1, 0))
         self.connect((self.freq_xlating_fir_filter_xxx_0_0, 0), (self.blocks_selector_1_0, 0))
         self.connect((self.freq_xlating_fir_filter_xxx_0_0, 0), (self.blocks_swapiq_0, 0))
+        self.connect((self.freq_xlating_fir_filter_xxx_0_0_0, 0), (self.rational_resampler_xxx_0_0_0_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))
         self.connect((self.low_pass_filter_1, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.low_pass_filter_1, 0), (self.blocks_sub_xx_0, 1))
         self.connect((self.low_pass_filter_1_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.low_pass_filter_1_0, 0), (self.blocks_sub_xx_0, 0))
-        self.connect((self.low_pass_filter_2_0, 0), (self.blocks_selector_1_0_0, 0))
-        self.connect((self.low_pass_filter_2_0, 0), (self.blocks_swapiq_0_0, 0))
+        self.connect((self.low_pass_filter_2, 0), (self.analog_agc2_xx_0, 0))
         self.connect((self.osmosdr_source_0, 0), (self.blocks_multiply_xx_0, 0))
         self.connect((self.osmosdr_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.osmosdr_source_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.analog_fm_demod_cf_0, 0))
         self.connect((self.rational_resampler_xxx_0_0, 0), (self.analog_agc2_xx_1, 0))
         self.connect((self.rational_resampler_xxx_0_0_0, 0), (self.analog_agc2_xx_1_0, 0))
-        self.connect((self.rational_resampler_xxx_0_0_0_0, 0), (self.analog_agc2_xx_1_0_0, 0))
-        self.connect((self.rational_resampler_xxx_1, 0), (self.band_pass_filter_1, 0))
+        self.connect((self.rational_resampler_xxx_0_0_0_0, 0), (self.band_pass_filter_0_0_0, 0))
+        self.connect((self.rational_resampler_xxx_0_0_0_0, 0), (self.blocks_swapiq_0_0, 0))
+        self.connect((self.rational_resampler_xxx_1, 0), (self.low_pass_filter_2, 0))
 
 
     def closeEvent(self, event):
@@ -511,14 +509,12 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_1_0.set_sampling_freq(self.samp_rate)
-        self.analog_sig_source_x_1_0_0.set_sampling_freq(self.samp_rate)
         self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.bandwidth, 25e3, window.WIN_HAMMING, 6.76))
         self.low_pass_filter_1.set_taps(firdes.low_pass(1, self.samp_rate, self.bandwidth, 500, window.WIN_HAMMING, 6.76))
         self.low_pass_filter_1_0.set_taps(firdes.low_pass(1, self.samp_rate, self.bandwidth, 500, window.WIN_HAMMING, 6.76))
         self.osmosdr_source_0.set_sample_rate(self.samp_rate)
         self.osmosdr_source_0.set_bandwidth(self.samp_rate, 0)
         self.qtgui_freq_sink_x_0.set_frequency_range(self.center_frequency, self.samp_rate)
-        self.qtgui_freq_sink_x_1.set_frequency_range(0, self.samp_rate)
         self.qtgui_waterfall_sink_x_0.set_frequency_range(self.center_frequency, self.samp_rate)
 
     def get_rf_gain(self):
@@ -545,7 +541,6 @@ class SDR_Software(gr.top_block, Qt.QWidget):
         self.analog_sig_source_x_0.set_frequency((self.center_frequency-self.channel_frequency))
         self.analog_sig_source_x_1.set_frequency(self.channel_frequency)
         self.analog_sig_source_x_1_0.set_frequency(self.channel_frequency)
-        self.analog_sig_source_x_1_0_0.set_frequency(self.channel_frequency)
 
     def get_center_frequency(self):
         return self.center_frequency
@@ -563,10 +558,13 @@ class SDR_Software(gr.top_block, Qt.QWidget):
     def set_bandwidth(self, bandwidth):
         self.bandwidth = bandwidth
         self.band_pass_filter_0.set_taps(firdes.band_pass(1, 1e6, 200, self.bandwidth, 500, window.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0_0.set_taps(firdes.complex_band_pass(1, 48e3, 200, (200+self.bandwidth), 500, window.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0_0_0.set_taps(firdes.complex_band_pass(1, 48e3, 200, (200+self.bandwidth), 150, window.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0_0_0_0.set_taps(firdes.complex_band_pass(1, 48e3, 200, (200+self.bandwidth), 150, window.WIN_HAMMING, 6.76))
         self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.bandwidth, 25e3, window.WIN_HAMMING, 6.76))
         self.low_pass_filter_1.set_taps(firdes.low_pass(1, self.samp_rate, self.bandwidth, 500, window.WIN_HAMMING, 6.76))
         self.low_pass_filter_1_0.set_taps(firdes.low_pass(1, self.samp_rate, self.bandwidth, 500, window.WIN_HAMMING, 6.76))
-        self.low_pass_filter_2_0.set_taps(firdes.low_pass(1, 1e6, self.bandwidth, 500, window.WIN_HAMMING, 6.76))
+        self.low_pass_filter_2.set_taps(firdes.low_pass(1, 48e3, self.bandwidth, 150, window.WIN_HAMMING, 6.76))
 
 
 
